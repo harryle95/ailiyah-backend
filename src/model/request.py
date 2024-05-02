b1da1a3eee095f892fdf08e2e5113ff88dbd5d7e
harryle <lehoangsonsg@gmail.com>
2024-05-02 12:25:46 +0930

    feat: separate request from prompt

--- src/model/request.py
+++ src/model/request.py
@@ -1,9 +1,14 @@
+from typing import TYPE_CHECKING
 from uuid import UUID
 
+from litestar.dto import dto_field
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model.base import Base
+
+if TYPE_CHECKING:
+    from src.model.prompt import Prompt
 
 __all__ = ("Request",)
 
@@ -11,7 +16,7 @@ __all__ = ("Request",)
 class Request(Base):
     __tablename__ = "request_table"  # type: ignore[assignment]
 
-    prompt: Mapped[str] = mapped_column(nullable=True)
-    input_image: Mapped[UUID] = mapped_column(nullable=True)
     output_image: Mapped[UUID] = mapped_column(nullable=True)
     project_id: Mapped[UUID] = mapped_column(ForeignKey("project_table.id", ondelete="CASCADE"))
+
+    prompts: Mapped[list["Prompt"]] = relationship(lazy="selectin", info=dto_field("read-only"), cascade="all, delete")
